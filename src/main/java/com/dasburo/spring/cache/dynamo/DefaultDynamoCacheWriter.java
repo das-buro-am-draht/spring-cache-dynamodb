9903c309c41239a376d673d67c69f992c32342c2
Daniel Teske <daniel.teske@europace.de>
2019-12-17 11:51:37 +0100

    changed ttl datatype from String to Number

--- src/main/java/com/dasburo/spring/cache/dynamo/DefaultDynamoCacheWriter.java
+++ src/main/java/com/dasburo/spring/cache/dynamo/DefaultDynamoCacheWriter.java
@@ -216,7 +216,7 @@ class DefaultDynamoCacheWriter implements DynamoCacheWriter {
     }
 
     if (shouldExpireWithin(ttl)) {
-      attributeValues.put(ATTRIBUTE_TTL, new AttributeValue().withS(String.valueOf(Instant.now().toEpochMilli() + ttl.toMillis())));
+      attributeValues.put(ATTRIBUTE_TTL, new AttributeValue().withN(String.valueOf(Instant.now().toEpochMilli() + ttl.toMillis())));
     }
 
     PutItemRequest putItemRequest = new PutItemRequest()
