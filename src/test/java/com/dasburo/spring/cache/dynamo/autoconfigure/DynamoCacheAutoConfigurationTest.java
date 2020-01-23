7cabb2d4ed639bcab7dc593d3d6262e520656e15
Daniel Teske <daniel.teske@europace.de>
2020-01-23 09:41:34 +0100

    added rootAttribute configuration for street attribute, according to test cases with Address

--- src/test/java/com/dasburo/spring/cache/dynamo/autoconfigure/DynamoCacheAutoConfigurationTest.java
+++ src/test/java/com/dasburo/spring/cache/dynamo/autoconfigure/DynamoCacheAutoConfigurationTest.java
@@ -14,7 +14,12 @@
  */
 package com.dasburo.spring.cache.dynamo.autoconfigure;
 
-import com.dasburo.spring.cache.dynamo.*;
+import com.dasburo.spring.cache.dynamo.DynamoCache;
+import com.dasburo.spring.cache.dynamo.DynamoCacheManager;
+import com.dasburo.spring.cache.dynamo.TestConfiguration;
+import com.dasburo.spring.cache.dynamo.TestDbCreationRule;
+import com.dasburo.spring.cache.dynamo.UnitTestBase;
+import com.dasburo.spring.cache.dynamo.rootattribute.RootAttributeConfig;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
@@ -24,6 +29,10 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.Duration;
+import java.util.List;
+
+import static com.amazonaws.services.dynamodbv2.model.ScalarAttributeType.S;
+import static java.util.Arrays.asList;
 
 /**
  * Unit tests for {@code DynamoCacheAutoConfiguration} class.
@@ -39,6 +48,7 @@ public class DynamoCacheAutoConfigurationTest extends UnitTestBase {
   private static final Duration TTL = Duration.ofSeconds(30);
   private static final Long READ_CAPACITY_UNITS = 1L;
   private static final Long WRITE_CAPACITY_UNITS = 1L;
+  private static final List<RootAttributeConfig> ROOT_ATTRIBUTES = asList(new RootAttributeConfig("street", S));
 
   @ClassRule
   public static TestDbCreationRule dynamoDB = new TestDbCreationRule();
@@ -54,7 +64,9 @@ public class DynamoCacheAutoConfigurationTest extends UnitTestBase {
       "spring.cache.dynamo.caches[0].cacheName:" + CACHE_NAME,
       "spring.cache.dynamo.caches[0].flushOnBoot:" + FLUSH_ON_BOOT,
       "spring.cache.dynamo.caches[0].readCapacityUnits:" + READ_CAPACITY_UNITS,
-      "spring.cache.dynamo.caches[0].writeCapacityUnits:" + WRITE_CAPACITY_UNITS
+      "spring.cache.dynamo.caches[0].writeCapacityUnits:" + WRITE_CAPACITY_UNITS,
+      "spring.cache.dynamo.caches[0].rootAttributes[0].name:" + ROOT_ATTRIBUTES.get(0).getName(),
+      "spring.cache.dynamo.caches[0].rootAttributes[0].type:" + ROOT_ATTRIBUTES.get(0).getType().name()
     );
   }
 
@@ -86,6 +98,8 @@ public class DynamoCacheAutoConfigurationTest extends UnitTestBase {
     Assert.assertEquals(TTL, cache.getTtl());
     Assert.assertEquals(CACHE_NAME, cache.getName());
     Assert.assertEquals(FLUSH_ON_BOOT, cache.isFlushOnBoot());
+    Assert.assertEquals(ROOT_ATTRIBUTES.get(0).getName(), cache.getRootAttributes().get(0).getName());
+    Assert.assertEquals(ROOT_ATTRIBUTES.get(0).getType(), cache.getRootAttributes().get(0).getType());
   }
 
 }
