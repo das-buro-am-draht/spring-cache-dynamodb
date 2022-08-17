07a40b47cac37f6a61ac37c860dd0ddf008c7eb9
derXear <xear@gmx.net>
2022-08-17 13:37:56 +0200

    test: Add unit test for TableUtils

---
+++ src/test/java/com/dasburo/spring/cache/dynamo/util/TableUtilsTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2019-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dasburo.spring.cache.dynamo.util;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.CreateTableRequest;
+import software.amazon.awssdk.services.dynamodb.model.ResourceInUseException;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class TableUtilsTest {
+
+  @Mock
+  private DynamoDbClient mockDynamoTemplate;
+
+  @Test
+  public void testCreateTableIfNotExists_alreadyExists() {
+    when(mockDynamoTemplate.createTable(any(CreateTableRequest.class)))
+      .thenThrow(ResourceInUseException.class);
+
+    boolean result = TableUtils.createTableIfNotExists(mockDynamoTemplate, CreateTableRequest.builder().build());
+    assertFalse(result);
+  }
+}
